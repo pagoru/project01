df392e1d025d639cd06d110f17d9a93a10a6683a
pagoru <pagoru@gmail.com>
2016-10-14 17:35:07 +0200

    Draw red lines
    - Can draw red lines
    - Integers to points

--- p01/Form1.vb
+++ p01/Form1.vb
@@ -1,20 +1,30 @@
 ﻿Public Class Form1
 
-    Dim firstX As Integer
-    Dim firstY As Integer
+    Dim first As Point
+    Dim last As Point
 
-    Dim lastX As Integer
-    Dim lastY As Integer
+    Dim mouseButton As String
 
     Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
-        firstX = e.X
-        firstY = e.Y
+        first = New Point(e.X, e.Y)
     End Sub
 
     Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
-        lastX = e.X
-        lastY = e.Y
+        last = New Point(e.X, e.Y)
 
-        Me.Text = CStr(firstX) + "-" + CStr(firstY) + " -> " + CStr(lastX) + "-" + CStr(lastY)
+        Me.Text = CStr(first.X) + "-" + CStr(first.Y) + " -> " + CStr(last.X) + "-" + CStr(last.Y)
+        DrawLinePoint(first, last)
     End Sub
+
+    Public Sub DrawLinePoint(ByVal first As Point, ByVal last As Point)
+
+        Dim myPen As New System.Drawing.Pen(System.Drawing.Color.Red)
+        Dim formGraphics As System.Drawing.Graphics
+        formGraphics = Me.CreateGraphics()
+        formGraphics.DrawLine(myPen, first.X, first.Y, last.X, last.Y)
+        myPen.Dispose()
+        formGraphics.Dispose()
+    End Sub
+
 End Class
+
