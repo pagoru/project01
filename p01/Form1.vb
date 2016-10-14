6e547de2b96b0f35955f3fd9a2dbc64da92d24b3
pagoru <pagoru@gmail.com>
2016-10-14 16:57:41 +0200

    First commit

---
+++ p01/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Dim firstX As Integer
+    Dim firstY As Integer
+
+    Dim lastX As Integer
+    Dim lastY As Integer
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        firstX = e.X
+        firstY = e.Y
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        lastX = e.X
+        lastY = e.Y
+
+        Me.Text = CStr(firstX) + "-" + CStr(firstY) + " -> " + CStr(lastX) + "-" + CStr(lastY)
+    End Sub
+End Class
